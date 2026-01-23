9dd2c575d3cd8ba69b1719dbc11fb2ef90b101d9
paulovictorarmando <paulovictorarmando@gmail.com>
2026-01-23 15:17:43 +0100

    .

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -2,7 +2,6 @@
 
 void Server::handler(int signal)
 {
-	(void)signal;
 	std::cout << "Server closed by signal: " << signal << std::endl;
 }
 
@@ -14,11 +13,23 @@ Server::Server(int port, const std::string& password)
 	pfd.fd = _serverFd;
 	pfd.events = POLLIN;
 	pfd.revents = 0;
-
 	_pollfds.push_back(pfd);
 }
 
-void Server::setupSocket()
+Server::~Server()
+{
+	for (std::map<int, Client*>::iterator it = _clients.begin();
+		it != _clients.end(); ++it)
+	{
+		close(it->first);
+		delete it->second;
+	}
+	_clients.clear();
+	if (_serverFd >= 0)
+		close(_serverFd);
+}
+
+void Server::setupSocket(void)
 {
 	if(this->_port > 65535 || this->_port < 1024)
 		throw std::invalid_argument("<port> must be between 1024 and 65535");
@@ -44,11 +55,11 @@ void Server::setupSocket()
 		throw std::runtime_error("fcntl() failed");
 }
 
-void Server::run()
+void Server::run(void)
 {
 	signal(SIGINT, handler); //^C
 	signal(SIGQUIT, handler); // "^\" ou ^|
-	signal(SIGTSTP, handler); // ^Z
+	//signal(SIGTSTP, handler); // ^Z
 	std::cout << "Server running in localhost port " << this->_port << std::endl;
 	while (true)
 	{
@@ -56,105 +67,127 @@ void Server::run()
 			break;
 		for (size_t i = 0; i < _pollfds.size(); ++i)
 		{
-			//std::cout <<" Client: " << _pollfds[i].fd << " reventes: " << _pollfds[i].revents << " And events: " << _pollfds[i].events << std::endl;
 			if (_pollfds[i].revents & POLLIN)
 			{
 				if (_pollfds[i].fd == _serverFd)
 					acceptClient();
 				else
-					receiveData(_pollfds[i].fd);
+					receiveData(i);
 			}
-
 			if (_pollfds[i].revents & POLLOUT)
-				sendData(_pollfds[i].fd);
+				sendData(i);
 		}
 	}
 }
 
-void Server::acceptClient()
+void Server::acceptClient(void)
 {
 	int clientFd = accept(_serverFd, NULL, NULL);
 	if (clientFd < 0)
-		return;
+		return;;
 
 	if (fcntl(clientFd, F_SETFL, O_NONBLOCK) < 0)
+	{
+		close(clientFd);
 		return;
-
+	}
 	struct pollfd pfd;
 	pfd.fd = clientFd;
 	pfd.events = POLLIN;
 	pfd.revents = 0;
-
 	_pollfds.push_back(pfd);
 	_clients[clientFd] = new Client(clientFd);
-
 	std::cout << "New client connected: " << clientFd << std::endl;
+	_clients[clientFd]->sendBuffer += welcome();
+	enablePollout(clientFd);
 }
 
-void Server::receiveData(int clientFd)
+void Server::receiveData(int indexFd)
 {
 	char buffer[512];
-	ssize_t bytes = recv(clientFd, buffer, sizeof(buffer), 0);
+	ssize_t bytes = recv(_pollfds[indexFd].fd, buffer, sizeof(buffer), 0);
 
 	if (bytes <= 0)
 	{
-		removeClient(clientFd);
+		if (bytes < 0)
+			std::cout << "Error in function recv()" << std::endl;
+		else
+			std::cout << "Client disconnected: " << _pollfds[indexFd].fd << std::endl;
+		removeClient(indexFd);
 		return;
 	}
-	_clients[clientFd]->recvBuffer.append(buffer, bytes);
+	_clients[_pollfds[indexFd].fd]->recvBuffer.append(buffer, bytes);
 }
 
-void Server::sendData(int clientFd)
+void Server::sendData(int indexFd)
 {
+	int clientFd = _pollfds[indexFd].fd;
 	Client* client = _clients[clientFd];
 	if (client->sendBuffer.empty())
 		return;
-
 	ssize_t bytes = send(clientFd,
 		client->sendBuffer.c_str(),
 		client->sendBuffer.size(),
 		0);
-	/*if (bytes < 0)
-     return;*/
 
 	if (bytes <= 0)
 	{
-		removeClient(clientFd);
+		if (bytes < 0)
+			std::cout << "Error in function send" << std::endl;
+		else
+			std::cout << "Client disconnected: " << clientFd << std::endl;
+		removeClient(indexFd);
 		return;
 	}
-
 	client->sendBuffer.erase(0, bytes);
+	if (client->sendBuffer.empty())
+		disablePollout(clientFd);
 }
 
-void Server::removeClient(int clientFd)
+void Server::removeClient(int indexFd)
 {
+	int clientFd = _pollfds[indexFd].fd;
 	close(clientFd);
-
-	for (size_t i = 0; i < _pollfds.size(); ++i)
-	{
-		if (_pollfds[i].fd == clientFd)
-		{
-			_pollfds.erase(_pollfds.begin() + i);
-			break;
-		}
-	}
+	_pollfds[indexFd];
+	_pollfds.erase(_pollfds.begin() + indexFd);
 
 	delete _clients[clientFd];
 	_clients.erase(clientFd);
-
-	std::cout << "Client disconnected: " << clientFd << std::endl;
 }
 
-
-Server::~Server()
+void Server::enablePollout(int fd)
 {
-	for (std::map<int, Client*>::iterator it = _clients.begin();
-		it != _clients.end(); ++it)
-	{
-		close(it->first);
-		delete it->second;
-	}
-	_clients.clear();
-	if (_serverFd >= 0)
-		close(_serverFd);
+    for (size_t i = 0; i < _pollfds.size(); ++i)
+    {
+        if (_pollfds[i].fd == fd)
+        {
+            _pollfds[i].events |= POLLOUT;
+            break;
+        }
+    }
 }
+
+void Server::disablePollout(int fd)
+{
+    for (size_t i = 0; i < _pollfds.size(); ++i)
+    {
+        if (_pollfds[i].fd == fd)
+        {
+            _pollfds[i].events &= ~POLLOUT;
+            break;
+        }
+    }
+}
+
+std::string	Server::welcome(void)
+{
+	std::string msg;
+	msg.append("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n");
+	msg.append("██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n");
+	msg.append("██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗\n");
+	msg.append("██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝\n");
+	msg.append("╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n");
+	msg.append(" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n");
+	msg.append("You need to login so you can start chatting\r\n");
+	return (msg);
+};
