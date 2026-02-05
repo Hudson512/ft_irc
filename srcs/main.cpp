a6ab7bed2e1843894543ca22b59538341baa342d
paulovictorarmando <paulovictorarmando@gmail.com>
2026-02-05 14:04:19 +0100

    Errors solved: empty password ("") and invalid ports (2345re)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,26 +1,44 @@
 #include "../includes/Server.hpp"
 #include <iostream>
 #include <cstdlib>
-
-int main(int ac, char **av)
+static bool ft_check(int ac, char **av)
 {
 	if (ac != 3)
 	{
 		std::cerr << "Usage: " << av[0] << " <port> <password>" << std::endl;
-		return 1;
+		return false;
 	}
+	for(int i = 0; av[1][i]; i++)
+	{
+		if(!std::isdigit(av[1][i]))
+		{
+			std::cerr << "Error: only numbers can be used for the <port> argument" << std::endl;
+			return false;
+		}
+	}
+	return true;	
+}
+
+int main(int ac, char **av)
+{
+	if (!ft_check(ac, av))
+		return 1;
 	int port = std::atoi(av[1]);
 	std::string password = av[2];
-	try
+	if (!password.empty())
 	{
-		Server server(port, password);
-		server.run();
+		try
+		{
+			Server server(port, password);
+			server.run();
+			return 0;
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+			return 1;
+		}
 	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-		return 1;
-	}
-
-	return 0;
+	std::cerr << "Error: the server password cannot be empty." << std::endl;
+	return 1;
 }
